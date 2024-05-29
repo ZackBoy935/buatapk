a505b73c73f66fbbf5d9e1f7a055640f0bbed413
ZackBoy935 <142200983+ZackBoy935@users.noreply.github.com>
2024-05-29 09:02:13 -0700

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+                           
+    $(".countdown").countdown({
+                date: "12 March 2027 18:30:00",
+                format: "on"
+            },
+            
+            function() {
+            });
+
+}); 
